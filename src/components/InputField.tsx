1697f563b783a295cd7fe56f07fd487e314d9405
Geane Moraes Ramos <geane.ramos@contavoltz.com>
2023-10-26 19:14:40 -0300

    CHORE: configurações e criação da interface do projeto

---
+++ src/components/InputField.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css';
+
+interface Props {
+    todo:string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>
+}
+
+const InputField = ({ todo, setTodo}: Props) => {
+  return (
+    <form className='input'>
+      <input type='input' placeholder='Enter a task' className='input_box'></input>
+      <button className='input_submit' type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
